ae5181197daa7000a463edb968c9e1eb643617f0
196 <kankou.chin@gmail.com>
2022-07-28 00:14:44 +0900

    add: layout & style

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,23 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import Head from 'next/head';
+import Script from 'next/script';
+import Layout from '../../components/layout';
+
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1>First Post</h1>
       <Image
         src="/images/profile.jpeg" // Route of the image file
@@ -20,6 +30,6 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
