4a96afd943453066ef31d440ec30ad3beabfd561
196 <kankou.chin@gmail.com>
2022-07-27 23:49:32 +0900

    add: image & head

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,20 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
+      <Image
+        src="/images/profile.jpeg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
       <h2>
         <Link href="/">
           <a>Back to home</a>
